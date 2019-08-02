245aa5ecf60ad63d67f617ebf16d863d214fa388
Samantha-Lisette Carelse <sam@carsal.co.za>
2019-08-02 09:50:32 +0200

    adding new files

---
+++ precache-manifest.0feefb5b8abf0641e9e7f05222fa1b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087ee092cf16c5aa365e12a417dcace5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "448514c0d8caec0e97e1",
+    "url": "/static/css/main.0761adec.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "448514c0d8caec0e97e1",
+    "url": "/static/js/main.ce91c336.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
